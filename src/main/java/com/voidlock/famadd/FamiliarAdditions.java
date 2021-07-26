478af8276452adef437f7ef34fc18d103fd8e064
VoidLock <Louisfredona@gmail.com>
2021-07-26 17:49:24 -0500

    Creating project files and setup

---
+++ src/main/java/com/voidlock/famadd/FamiliarAdditions.java
@@ -0,0 +1,11 @@
+package com.voidlock.famadd;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FamiliarAdditions implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
